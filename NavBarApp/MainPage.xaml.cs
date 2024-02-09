567815e2de42a00bcbfd4e40b48a329d7a57fd04
Steve Bilogan <steve.bilogan@gmail.com>
2024-02-08 22:15:15 -0500

    primary commands

--- NavBarApp/MainPage.xaml.cs
+++ NavBarApp/MainPage.xaml.cs
@@ -2,13 +2,28 @@ namespace NavBarApp;
 
 public sealed partial class MainPage : Page
 {
+    public int NotificationCount { get; set; } = 0;
+    public int ShareCount { get; set; } = 0;
     public MainPage()
     {
         this.InitializeComponent();
+        
+        NotifCountRun.Text = NotificationCount.ToString();
+        ShareCountRun.Text = ShareCount.ToString();
     }
 
     public void NavBtn_Click(object sender, object args)
     {
         Frame.Navigate(typeof(SecondPage));
+    }
+
+    public void Notification_Click(object sender, object args)
+    {
+        NotifCountRun.Text = (++NotificationCount).ToString();
+    }
+
+    public void Share_Click(object sender, object args)
+    {
+         ShareCountRun.Text = (++ShareCount).ToString();
     }
 }
--- NavBarApp/SecondPage.xaml.cs
+++ NavBarApp/SecondPage.xaml.cs
@@ -2,9 +2,27 @@
 
 public sealed partial class SecondPage : Page
 {
+    private int _rotation = 0;
     public SecondPage()
     {
         this.InitializeComponent();
+    }
+
+    public void Notification_Click(object sender, object args)
+    {
+        Rotate(-15);
+    }
+
+    public void Share_Click(object sender, object args)
+    {
+         Rotate(15);
+    }
+
+    private void Rotate(int step)
+    {
+        _rotation = (_rotation + step) % 360;
+
+        myRotate.Angle = _rotation;
     }
 }
 
